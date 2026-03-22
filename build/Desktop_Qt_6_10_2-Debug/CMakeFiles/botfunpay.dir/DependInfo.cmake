db57942a1026dd5ffa781e8426a5aa5b0bfc2489
creat4got <sadykovdanielfgt@gmail.com>
2026-03-22 22:33:44 +0500

    Add files via upload

---
+++ build/Desktop_Qt_6_10_2-Debug/CMakeFiles/botfunpay.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "botfunpay_autogen/timestamp" "custom" "botfunpay_autogen/deps"
+  "/home/undersirable/botfunpay/addlotdialog.cpp" "CMakeFiles/botfunpay.dir/addlotdialog.cpp.o" "gcc" "CMakeFiles/botfunpay.dir/addlotdialog.cpp.o.d"
+  "/home/undersirable/botfunpay/botengine.cpp" "CMakeFiles/botfunpay.dir/botengine.cpp.o" "gcc" "CMakeFiles/botfunpay.dir/botengine.cpp.o.d"
+  "/home/undersirable/botfunpay/build/Desktop_Qt_6_10_2-Debug/botfunpay_autogen/EWIEGA46WW/qrc_resources.cpp" "CMakeFiles/botfunpay.dir/botfunpay_autogen/EWIEGA46WW/qrc_resources.cpp.o" "gcc" "CMakeFiles/botfunpay.dir/botfunpay_autogen/EWIEGA46WW/qrc_resources.cpp.o.d"
+  "/home/undersirable/botfunpay/build/Desktop_Qt_6_10_2-Debug/botfunpay_autogen/mocs_compilation.cpp" "CMakeFiles/botfunpay.dir/botfunpay_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/botfunpay.dir/botfunpay_autogen/mocs_compilation.cpp.o.d"
+  "/home/undersirable/botfunpay/botworker.cpp" "CMakeFiles/botfunpay.dir/botworker.cpp.o" "gcc" "CMakeFiles/botfunpay.dir/botworker.cpp.o.d"
+  "/home/undersirable/botfunpay/cookieloader.cpp" "CMakeFiles/botfunpay.dir/cookieloader.cpp.o" "gcc" "CMakeFiles/botfunpay.dir/cookieloader.cpp.o.d"
+  "/home/undersirable/botfunpay/dashboardwindow.cpp" "CMakeFiles/botfunpay.dir/dashboardwindow.cpp.o" "gcc" "CMakeFiles/botfunpay.dir/dashboardwindow.cpp.o.d"
+  "/home/undersirable/botfunpay/funpayapi.cpp" "CMakeFiles/botfunpay.dir/funpayapi.cpp.o" "gcc" "CMakeFiles/botfunpay.dir/funpayapi.cpp.o.d"
+  "/home/undersirable/botfunpay/funpayclient.cpp" "CMakeFiles/botfunpay.dir/funpayclient.cpp.o" "gcc" "CMakeFiles/botfunpay.dir/funpayclient.cpp.o.d"
+  "/home/undersirable/botfunpay/gamesparser.cpp" "CMakeFiles/botfunpay.dir/gamesparser.cpp.o" "gcc" "CMakeFiles/botfunpay.dir/gamesparser.cpp.o.d"
+  "/home/undersirable/botfunpay/gamesservice.cpp" "CMakeFiles/botfunpay.dir/gamesservice.cpp.o" "gcc" "CMakeFiles/botfunpay.dir/gamesservice.cpp.o.d"
+  "/home/undersirable/botfunpay/httpclient.cpp" "CMakeFiles/botfunpay.dir/httpclient.cpp.o" "gcc" "CMakeFiles/botfunpay.dir/httpclient.cpp.o.d"
+  "/home/undersirable/botfunpay/logger.cpp" "CMakeFiles/botfunpay.dir/logger.cpp.o" "gcc" "CMakeFiles/botfunpay.dir/logger.cpp.o.d"
+  "/home/undersirable/botfunpay/main.cpp" "CMakeFiles/botfunpay.dir/main.cpp.o" "gcc" "CMakeFiles/botfunpay.dir/main.cpp.o.d"
+  "/home/undersirable/botfunpay/mainwindow.cpp" "CMakeFiles/botfunpay.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/botfunpay.dir/mainwindow.cpp.o.d"
+  "/home/undersirable/botfunpay/settingsmanager.cpp" "CMakeFiles/botfunpay.dir/settingsmanager.cpp.o" "gcc" "CMakeFiles/botfunpay.dir/settingsmanager.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/Desktop_Qt_6_10_2-Debug/CMakeFiles/botfunpay.dir/botfunpay_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/botfunpay.dir/botfunpay_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
+ /home/undersirable/botfunpay/build/Desktop_Qt_6_10_2-Debug/botfunpay_autogen/EWIEGA46WW/qrc_resources.cpp \
+ /usr/include/stdc-predef.h
